8aeece7fbe38f0ec301e7f9def4fcf9e59ef055b
dnstrn <denistrinh@gmail.com>
2016-04-29 10:00:25 -0700

    add a method called info

--- practice.rb
+++ practice.rb
@@ -4,4 +4,10 @@ class Car
 
   def initialize(color, capacity)
     @color, @capacity = color, capacity
+  end
+
+  def info
+    "This car has a capacity of #{@capacity} and #{@color} color"
+  end
+
 end
